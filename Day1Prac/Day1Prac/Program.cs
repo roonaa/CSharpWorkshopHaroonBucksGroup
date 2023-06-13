59793aeaf5dcd41f14f89068623020ccda7ef415
roonaa <91068807+roonaa@users.noreply.github.com>
2023-06-13 12:15:11 +0100

    Added a piece of code

---
+++ Day1Prac/Day1Prac/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+// Create a string variable
+string myName = "Sara";
+//Console.WriteLine(myName);
+
+// The equal is the assignment and the writing after that is the data
+
+// Creating another variable
+string movieName = "House IV";
+string secondMovieName = "House V";
+//Console.WriteLine(movieName);
+
+// Create a variable to store an integer
+int myAge = 19;
+//Console.WriteLine(myAge);
+
+// Create a decimal variable
+
+decimal myMoney = 100.50m;
+//Console.WriteLine(myMoney);
+
+// Create a boolean variable
+bool isRaining = false;
+//Console.WriteLine(isRaining);
+
+// Create a double variable
+float temp = 26.5f;
+//Console.WriteLine(temp);
+
+// Building the output string
+string outputString = "This Thursday, I am going to be watching " + movieName;
+string outputString2 = "This Friday, I am going to be watching " + secondMovieName;
+
+Console.WriteLine(outputString);
+Console.WriteLine(outputString2);
