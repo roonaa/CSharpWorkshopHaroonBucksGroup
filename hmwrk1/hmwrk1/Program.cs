1d2d9fb3529bedc37fb9ee6f1e866970afdbe679
roonaa <91068807+roonaa@users.noreply.github.com>
2023-06-15 08:44:10 +0100

    hmwrk

---
+++ hmwrk1/hmwrk1/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices;
+
+string tvShow = "The Simpsons";
+Console.WriteLine("One of the longest running animated tv show is " +  tvShow);
+
+string app = "Google Maps";
+Console.WriteLine("A application I have on my phone is " +  app);
+
+int age = 89;
+Console.WriteLine(age);
+
+int bedrooms = 5;
+Console.WriteLine(bedrooms);
+
+double temp = 26.5;
+Console.WriteLine("The temperature outside is " + temp);
+
+double cost = 1.35;
+Console.WriteLine("The cost of a chocolate bar is £" + cost);
+
+Console.WriteLine("He is " + age + " years old and has a " + bedrooms + " bedroom house");
