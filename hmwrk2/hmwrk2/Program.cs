9af706db772b1bc2801fb85d7c0af3b9cfc4a5f0
roonaa <91068807+roonaa@users.noreply.github.com>
2023-06-20 09:26:20 +0100

    hmwrk2

---
+++ hmwrk2/hmwrk2/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// This adds up all of the numbers
+using System.Runtime.InteropServices;
+
+int addthenumbers(int num1, int num2, int num3, int num4)
+{
+    int sum = num1 + num2 + num3 + num4;
+    return sum;
+}
+
+// This calls the function
+int thesumoffour = addthenumbers(21, 22, 23, 24);
+
+// This prints the result
+Console.WriteLine("21 + 22 + 23 + 24 = " + thesumoffour);
