bf1c50de0e01c65267e094cc710baccf8d256622
roonaa <91068807+roonaa@users.noreply.github.com>
2023-06-27 12:05:36 +0100

    Day 5
    Classes and Functions in classes

---
+++ Day5Prac/Day5Prac/Program.cs
@@ -0,0 +1,67 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Day5Prac;
+
+Console.WriteLine("Hello, World!");
+
+#region old code format
+/*// student called 
+Student Pregozhin = new Student();
+Student Alexandria = new Student();
+
+// Assigning the students to numbers
+Pregozhin.NameOfStudent = "Pregozhin";
+Pregozhin.StudyHoursPerWeek= 35;
+Pregozhin.StudyWeeksCompleted= 0;
+
+Alexandria.NameOfStudent = "Alexandria";
+Alexandria.StudyHoursPerWeek = 15;
+Alexandria.StudyWeeksCompleted = 10;
+
+string outputforPreg = $" Student Name : {Pregozhin.NameOfStudent} Study Hours : {Pregozhin.StudyHoursPerWeek} Number of weeks Completed : {Pregozhin.StudyWeeksCompleted}";
+
+string outputforAlex = $" Student Name : {Alexandria.NameOfStudent} Study Hours : {Alexandria.StudyHoursPerWeek} Number of weeks Completed : {Alexandria.StudyWeeksCompleted}";
+
+Console.WriteLine(outputforPreg);
+Console.WriteLine(outputforAlex);*/
+#endregion
+
+#region New coding format
+// Student Pregozhin
+Student Pregozhin = new Student();
+Pregozhin.NameOfStudent = "Pregozhin";
+Pregozhin.StudyHoursPerWeek = 25;
+Pregozhin.StudyWeeksCompleted = 0;
+Pregozhin.BuildStudentInformation();
+Pregozhin.DisplayStudentInformation();
+
+// Student Alexandria
+Student Alexandria = new Student();
+Alexandria.NameOfStudent = "Alexandria";
+Alexandria.StudyHoursPerWeek = 10;
+Alexandria.StudyWeeksCompleted = 10;
+Alexandria.BuildStudentInformation();
+Alexandria.DisplayStudentInformation();
+
+// Student Russell
+Student Russell = new Student();
+Russell.NameOfStudent = "Russell";
+Russell.StudyHoursPerWeek = 30;
+Russell.StudyWeeksCompleted = 35;
+Russell.BuildStudentInformation();
+Russell.DisplayStudentInformation();
+#endregion
+
+// It takes 500 hours to become a beginner developer
+int TotalBeginnerDeveloperHours = 500;
+
+/// 20 hours per week
+/// 25 weeks to become a beginner developer
+/// number of weeks already competed is 0
+/// remaining weeks = 25 - 0 = 25 weeks
+/// Ultimately, Pregozhin will become a developer in 25 weeks
+
+/// 10 hours per week
+/// 50 weeks to become a beginner developer
+/// number of weeks already completed is 10
+/// remaning weeks = 50 - 10 = 40 weeks
+/// Ultimately, Alexandria will become a developer in 40 weeks
---
+++ Day5Prac/Day5Prac/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5Prac
+{
+    public class Student
+    {
+        // Fields or Properties or Variables
+        public string NameOfStudent { get; set; }
+        public int StudyHoursPerWeek { get; set; }
+        public int StudyWeeksCompleted { get; set; }
+        //Stirng to be able to share the output
+        public string OutputStorage { get; set; }
+        // Functions or Methods or Behaviours or Actions
+        public void BuildStudentInformation()
+        {
+            this.OutputStorage = $" Student Name : {this.NameOfStudent} Study Hours : {this.StudyHoursPerWeek} Number of weeks Completed : {this.StudyWeeksCompleted}";
+        }
+        public void DisplayStudentInformation()
+        {
+            Console.WriteLine(this.OutputStorage);
+        }
+    }
+}
